93f9c46515339676d4c65159a646c68af61d6e12
Gergő Csontos <gergo.csontos.123@gmail.com>
2022-01-10 19:37:00 +0100

    feat: add NavBar to App

--- el-proyecte-grande-frontend/src/components/App.js
+++ el-proyecte-grande-frontend/src/components/App.js
@@ -1,9 +1,12 @@
 import '../css/App.css';
+import NavBar from '../components/NavBar';
 
 function App() {
   return (
     <>
+    <NavBar title="I'm the title"></NavBar>
     <h1>Hello Mavens!</h1>
+    <div className='randomContent'></div>
     </>
   );
 }
