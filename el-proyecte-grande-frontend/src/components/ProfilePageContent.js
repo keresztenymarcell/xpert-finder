d9527dcb41d2c526a8e1b0403139e3c730430051
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-01-12 15:39:07 +0100

    feat: add Profile Page and Service components

--- el-proyecte-grande-frontend/src/components/ProfilePageContent.js
+++ el-proyecte-grande-frontend/src/components/ProfilePageContent.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import Service from './Service';
 
 function ProfilePageContent() {
 
@@ -6,8 +7,37 @@ function ProfilePageContent() {
 
     return (
         <div className="content-container">
-            <h1>I'M the profile Page</h1>
-            <p>{params.id}</p>
+            <div className="profile-page-card">
+                <div className="left-container">
+                    <img className="profile-picture" src="profile-picture.jpeg" alt="profile-picture"></img>
+                    <h1>John Doe</h1>
+                    <p>Budapest, Gyor, Szeged</p>
+                    <h1>Reviewed X times</h1>
+                    <p>5.0</p>
+                    <p>{params.id}</p>
+                </div>
+
+                <div className="right-container">
+                    <h1>About me</h1>
+                    <div className="description">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h3>
+                    </div>
+
+                    <h1>Services</h1>
+                    <div className="services">
+                        <Service></Service>
+                    </div>
+                    
+
+                    
+                </div>
+
+                
+
+
+
+            </div>
+            
         </div>
     )
 }
