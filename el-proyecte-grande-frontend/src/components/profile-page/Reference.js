56f1d301e706a18b8130e85622a4b4f919307c9e
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-01-13 10:20:48 +0100

    feat: add Reference Component with styleing

---
+++ el-proyecte-grande-frontend/src/components/profile-page/Reference.js
@@ -0,0 +1,15 @@
+
+
+const Reference = () => {
+    return (
+        <div className="reference">
+            <div className="reference-picture-container">
+                <img className="reference-picture" src="profile-picture.jpeg" alt="profile-picture"></img>
+            </div>
+            <p>This is my reference description</p>
+            
+        </div>
+    )
+}
+
+export default Reference
