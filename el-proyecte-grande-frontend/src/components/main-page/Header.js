8ddf0985554e66a45081a4dad9b336a32be27c5d
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-01-13 18:39:58 +0100

    feat: fix typo

--- el-proyecte-grande-frontend/src/components/main-page/Header.js
+++ el-proyecte-grande-frontend/src/components/main-page/Header.js
@@ -18,7 +18,7 @@ const Header = ({user, setUser}) => {
                     <li><Link to="/">Home</Link></li>
                     {(() => {
                         if (user == null){
-                           return <li><Link to="/sing-in" onClick={() => {setUser('placeholderUser')}}>Sing in</Link></li> 
+                           return <li><Link to="/sing-in" onClick={() => {setUser('placeholderUser')}}>Sign in</Link></li> 
                         }
                         else{
                            return <li><Link to="/log-out" onClick={() => {setUser(null)}}>Log out</Link></li> 
