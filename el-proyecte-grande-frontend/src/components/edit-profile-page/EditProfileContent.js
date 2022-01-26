e0972c9aea8a7dc3d617e5b4e16e1d964b4908ed
gergocsontos <gergo.csontos.123@gmail.com>
2022-01-26 18:27:54 +0100

    feat: add personal info inputs

--- el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
+++ el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
@@ -4,18 +4,27 @@ function EditProfileContent({userId, professions, locations}) {
 
     const [userData, setUserData] = useState(null);
 
+    let updatedUser = userData;
+
     useEffect(()=>{
         async function getUserData() {
             if (userId != null) {
                 const response = await fetch(`/user/${userId}`)
                 const data = await response.json();
                 setUserData(data);
-            }
-            
+                updatedUser = data;
+                
+            }   
         }
-        
         getUserData();
     }, [userId])
+
+
+    function updateHomeLocation(id) {
+        const location = locations.find(location => location.id === parseInt(id))
+        updatedUser.personalInfo.location = location;
+        setUserData(updatedUser)
+    }
 
     return (
         <div className="content-container simple-content-container">
@@ -28,11 +37,48 @@ function EditProfileContent({userId, professions, locations}) {
                         <p>Name</p>
                         <input type="text" placeholder={userData.personalInfo.name}></input>
                     </label>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" placeholder={userData.personalInfo.username}></input>
+                    </label>
+                    <label>
+                        <p>Email</p>
+                        <input type="email" placeholder={userData.personalInfo.email}></input>
+                    </label>
+                    <label>
+                        <p>Phone Number</p>
+                        <input type="text" placeholder={userData.personalInfo.phoneNumber}></input>
+                    </label>
+                    <label>
+                        <p>Profile Picture Url</p>
+                        <input type="text" placeholder={userData.personalInfo.profilePicture}></input>
+                    </label>
+                    <label>
+                        <p>Location</p>
+                        
+                        <select id="home-locations" defaultValue={userData.personalInfo.location.name} onChange={(e)=> {
+                            updateHomeLocation(e.target.value)
+                        }}>
+                            {locations && 
+                                locations.map(location => {return <option key={location.id} value={location.id}>{location.name}</option>})
+                            }
+                        </select>
+                    </label>
+                </form>
+                {userData.expertInfo &&
+                <>
+                    <form>
+                    <h2>Expert Info</h2>
+                    <label>
+                        <p>Description</p>
+                        <textarea placeholder={userData.expertInfo.description}></textarea>
+                    </label>
+                    <h2>Locations</h2>
                     
-                </form>
-                <form>
 
-                </form>
+                    </form>
+                </>
+            }
             </>
 }
         </div>
