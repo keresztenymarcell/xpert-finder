6eecd5a4ed63b6c58610965257e9e623704b7cc3
gergocsontos <gergo.csontos.123@gmail.com>
2022-01-27 08:36:37 +0100

    feat: make all personalInfo fields dynamic

--- el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
+++ el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
@@ -4,12 +4,15 @@ function EditProfileContent({userId, professions, locations}) {
 
     const [userData, setUserData] = useState(null);
 
+    const [updatedUserData, setUpdatedUserData] = useState(null)
+
     useEffect(()=>{
         async function getUserData() {
             if (userId != null) {
                 const response = await fetch(`/user/${userId}`)
                 const data = await response.json();
                 setUserData(data);
+                setUpdatedUserData(data);
             }   
         }
         getUserData();
@@ -24,36 +27,46 @@ function EditProfileContent({userId, professions, locations}) {
     return (
         <div className="content-container simple-content-container">
             <h1>Edit Page</h1>
-            <h5>{JSON.stringify(userData)}</h5>
-            {userData &&
+            <h5>{JSON.stringify(updatedUserData)}</h5>
+            {updatedUserData &&
             <>
                 <form>
                     <h2>Personal Info</h2>
                     <label>
                         <p>Name</p>
-                        <input type="text" placeholder={userData.personalInfo.name}></input>
+                        <input type="text" placeholder={updatedUserData.personalInfo.name} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, name: e.target.value}})
+                        }}></input>
                     </label>
                     <label>
                         <p>Username</p>
-                        <input type="text" placeholder={userData.personalInfo.username}></input>
+                        <input type="text" placeholder={updatedUserData.personalInfo.username} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, username: e.target.value}})
+                        }}></input>
                     </label>
                     <label>
                         <p>Email</p>
-                        <input type="email" placeholder={userData.personalInfo.email}></input>
+                        <input type="email" placeholder={updatedUserData.personalInfo.email} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, email: e.target.value}})
+                        }}></input>
                     </label>
                     <label>
                         <p>Phone Number</p>
-                        <input type="text" placeholder={userData.personalInfo.phoneNumber}></input>
+                        <input type="text" placeholder={updatedUserData.personalInfo.phoneNumber} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, phoneNumber: e.target.value}})
+                        }}></input>
                     </label>
                     <label>
                         <p>Profile Picture Url</p>
-                        <input type="text" placeholder={userData.personalInfo.profilePicture}></input>
+                        <input type="text" placeholder={updatedUserData.personalInfo.profilePicture} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, profilePicture: e.target.value}})
+                        }}></input>
                     </label>
                     <label>
                         <p>Location</p>
                         
-                        <select id="home-locations" defaultValue={userData.personalInfo.location.id} onChange={(e)=> {
-                            setUserData({...userData, personalInfo:{...userData.personalInfo, location:getLocationById(e.target.value)}})
+                        <select id="home-locations" defaultValue={updatedUserData.personalInfo.location.id} onChange={(e)=> {
+                            setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, location:getLocationById(e.target.value)}})
                         }}>
                             {locations && 
                                 locations.map(location => {return <option key={location.id} value={location.id}>{location.name}</option>})
@@ -61,13 +74,15 @@ function EditProfileContent({userId, professions, locations}) {
                         </select>
                     </label>
                 </form>
-                {userData.expertInfo &&
+                {updatedUserData.expertInfo &&
                 <>
                     <form>
                     <h2>Expert Info</h2>
                     <label>
                         <p>Description</p>
-                        <textarea placeholder={userData.expertInfo.description}></textarea>
+                        <textarea defaultValue={updatedUserData.expertInfo.description} onChange={e => {
+                            setUpdatedUserData({...updatedUserData, expertInfo:{...updatedUserData.expertInfo, description:e.target.value}})
+                        }}></textarea>
                     </label>
                     <h2>Locations</h2>
                     
