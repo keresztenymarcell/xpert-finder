8a617a651306e2922a4fce80e4aa7225e94df83d
gergocsontos <gergo.csontos.123@gmail.com>
2022-02-17 11:16:17 +0100

    feat: begin refactoring UpdateExpertInfo

--- el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
+++ el-proyecte-grande-frontend/src/components/edit-profile-page/EditProfileContent.js
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom"
 import SelectLocation from "./SelectLocation";
-import SelectLocationContainer from "./SelectLocationContainer";
-import SelectProfessionContainer from "./SelectProfessionContainer";
-import UpdateServicesContainer from "./UpdateServicesContainer";
 import {Login} from "../register-page/Login.css"
 import UserService from "../service/UserService";
-import UpdateReferencesContainer from "./UpdateReferencesContainer";
+import UpdateExpertInfo from "./UpdateExpertInfo";
 
 function EditProfileContent({user, professions, locations}) {
 
@@ -42,51 +39,9 @@ function EditProfileContent({user, professions, locations}) {
         return location;
     }
 
-    function getProfessionById(id) {
-        const profession = professions.find(profession => profession.id === id)
-        console.log(profession)
-        return profession;
-    }
-
     function updateHomeLocation(target) {
         const id = parseInt(target.value)
         setUpdatedUserData({...updatedUserData, personalInfo:{...updatedUserData.personalInfo, location:getLocationById(id)}})
-    }
-
-    function updateWorkLocation(target) {
-        const id = parseInt(target.value);
-        const index = parseInt(target.dataset.index);
-        const chosenLocation = getLocationById(id);
-
-        let newLocations = updatedUserData.expertInfo.locations;
-
-        if (!isItemAlreadyInArray(chosenLocation, newLocations)) {
-            newLocations[index] = chosenLocation;
-        } else {
-            newLocations.splice(index, 1)
-        }
-        setUpdatedUserData({...updatedUserData, expertInfo:{...updatedUserData.expertInfo, locations:newLocations}})
-        
-    }
-
-    function updateProfession(target) {
-        const id = parseInt(target.value);
-        const index = parseInt(target.dataset.index);
-        const chosenProfession = getProfessionById(id);
-
-        let newProfessions = updatedUserData.expertInfo.professions;
-
-        if (!isItemAlreadyInArray(chosenProfession, newProfessions)) {
-            newProfessions[index] = chosenProfession;
-        } else {
-            newProfessions.splice(index, 1)
-        }
-        setUpdatedUserData({...updatedUserData, expertInfo:{...updatedUserData.expertInfo, professions:newProfessions}})
-        
-    }
-
-    function isItemAlreadyInArray(item, array) {
-        return array.map(item => item.id).includes(item.id)
     }
 
     return (
@@ -132,31 +87,7 @@ function EditProfileContent({user, professions, locations}) {
                     <SelectLocation firstValue={updatedUserData.personalInfo.location.id} locations={locations} updateLocation={updateHomeLocation}/>
                 </form>
                 
-                {updatedUserData.expertInfo &&
-                <>
-                    <form>
-                    <h2 className="form-title">Expert Info</h2>
-                    <label className="form-label">
-                        <p>Description</p>
-                        <textarea defaultValue={updatedUserData.expertInfo.description} onChange={e => {
-                            setUpdatedUserData({...updatedUserData, expertInfo:{...updatedUserData.expertInfo, description:e.target.value}})
-                        }}></textarea>
-                    </label>
-                    <h2>Locations</h2>
-                    <h5>{JSON.stringify(updatedUserData.expertInfo.locations)}</h5>
-                    <SelectLocationContainer updatedUserData={updatedUserData} setUpdatedUserData={setUpdatedUserData} updateLocation={updateWorkLocation} locations={locations} />
-                        
-                    <h2>Professions</h2>
-                    <h5>{JSON.stringify(updatedUserData.expertInfo.professions)}</h5>
-                    <SelectProfessionContainer updatedUserData={updatedUserData} setUpdatedUserData={setUpdatedUserData} updateProfession={updateProfession} professions={professions} />
-                    <h2>Services</h2>
-                    <UpdateServicesContainer updatedUserData={updatedUserData} setUpdatedUserData={setUpdatedUserData} />
-                    <h2>References</h2>
-                    <UpdateReferencesContainer updatedUserData={updatedUserData} setUpdatedUserData={setUpdatedUserData} />
-                    
-                    </form>
-                </>
-            }
+                <UpdateExpertInfo locations={locations} professions={professions} getLocationById={getLocationById} />
             <button className={"submit"} onClick={()=> {handleSubmit()}}>Save Changes</button>
             </>
 }
